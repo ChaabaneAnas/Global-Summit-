36393a5b3da2fb50a59b817b9dc3a9b32f950b6a
ChaabaneAnas <anas.chaabane98@gmail.com>
2022-06-29 09:09:14 +0100

    mobile version + mobile menu

---
+++ menu.js
@@ -0,0 +1,9 @@
+ const hamburger = document.querySelector('.hamburger')
+ const navMenu = document.querySelector('.dNav')
+
+ hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+ })
+
+ console.log(navMenu);
